ffc85642bb42a7b3a56d52b2325d22ac25bf9515
itiya <fragment.vertex@gmail.com>
2018-07-08 22:22:27 +0900

    [add] keyとresponseのカラムを選択できるよう

--- lib/scrapbox_response_table.rb
+++ lib/scrapbox_response_table.rb
@@ -3,16 +3,18 @@ require "scrapbox_table_fetcher"
 
 module ScrapboxResponseTable
   class ResponseFetcher
-    def initialize(project, page, table)
+    def initialize(project, page, table, key_column, response_column)
       @project = project
       @page = page
       @table = table
+      @key_column = key_column
+      @response_column = response_column
       @fetcher = ScrapboxTableFetcher::Fetcher.new(project)
     end
 
     def response(key)
       @target ||= @fetcher.fetch(@page).select {|table| table.name == @table}[0]
-      @target.search_row('key', key)[0]['url']
+      @target.search_row(@key_column, key)[0][@response_column]
     end
   end
 end
